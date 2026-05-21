d63b615de59420c41a4b48f9deaa3359791bb7f7
ALBERT-MF <150465631+ALBERT-MF@users.noreply.github.com>
2026-05-21 23:33:08 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'vins-v2';
+const FITXERS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Instal·lació: guardem tots els fitxers a la caché
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FITXERS))
+  );
+  self.skipWaiting();
+});
+
+// Activació: eliminem caché antiga si n'hi ha
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: primer mirem la caché, si no hi és anem a la xarxa
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
